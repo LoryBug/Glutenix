0fe9cf68ba0915f1ff38f4b99981e3bb4da10076
LoryBug <72506393+LoryBug@users.noreply.github.com>
2026-06-14 12:34:25 +0200

    perf(P3): reduce GPR test from 300/100 to 80/30 samples/iterations for speed

--- tests/test_gpr.py
+++ tests/test_gpr.py
@@ -18,14 +18,14 @@ def seeded_session(db_session):
 def test_gpr_training(seeded_session):
     ingredients = seeded_session.query(Ingredient).all()
 
-    train_x, train_y = generate_synthetic_data(ingredients, n_samples=300)
-    train_y += torch.randn(300) * 0.5
+    train_x, train_y = generate_synthetic_data(ingredients, n_samples=80)
+    train_y += torch.randn(80) * 0.5
 
-    assert train_x.shape == (300, 9)
-    assert train_y.shape == (300,)
+    assert train_x.shape == (80, 9)
+    assert train_y.shape == (80,)
 
     gpr = PhysicsGPR()
-    gpr.train(train_x, train_y, n_iter=100, verbose=False)
+    gpr.train(train_x, train_y, n_iter=30, verbose=False)
     assert gpr.is_trained
 
     pred = gpr.predict(train_x[0].tolist())
@@ -33,9 +33,9 @@ def test_gpr_training(seeded_session):
     assert pred.std > 0
     assert pred.conf_interval_95[0] < pred.conf_interval_95[1]
 
-    means, stds = gpr.predict_batch(train_x[:10])
-    assert means.shape == (10,)
-    assert stds.shape == (10,)
+    means, stds = gpr.predict_batch(train_x[:5])
+    assert means.shape == (5,)
+    assert stds.shape == (5,)
 
     ood_x = torch.tensor([[50.0, 90.0, 30.0, 40.0, 5.0, 80.0, 30.0, 5.0, 0.5]])
     ood_pred = gpr.predict(ood_x[0].tolist())
